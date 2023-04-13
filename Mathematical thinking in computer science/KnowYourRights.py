ee7a2e707a4f2af83e36643efdf18ecd0ddd3f92
IBRAHIM REFAT <101995954+ibrahimrifatcse@users.noreply.github.com>
2023-04-13 11:10:32 +0600

    Update KnowYourRights.py

--- Mathematical thinking in computer science/KnowYourRights.py
+++ Mathematical thinking in computer science/KnowYourRights.py
@@ -1,4 +1,5 @@
 #a two-digit number that becomes 7 times smaller after the first digit is deleted
+#naive way
 for i in range( 1,100):
     x = i%10
     if(x*7==i):
@@ -8,5 +9,17 @@ for i in range(1000,10000):
     n=i%1000
     if(n*57==i):
         print(i," ",n)
+
+#effecient way 
+# for the two-digit number problem
+for i in range(10, 100):
+    x, y = divmod(i, 10)
+    if x*7 == 10*y+x:
+        print(i)
         
+# for the four-digit number problem
+for i in range(1000, 10000):
+    x, y = divmod(i, 1000)
+    if x*57 == 1000*y+x:
+        print(i)
         
